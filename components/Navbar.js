4ccf9dcd0c677601bc063003256e5f677a860048
Hozefa Mangalorewala <hozefam@gmail.com>
2018-09-17 00:00:11 +0300

    Initial Checkin

---
+++ components/Navbar.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="#">
+      BlitzPrice
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarColor01"
+      aria-controls="navbarColor01"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarColor01">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">
+            About
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Navbar;
